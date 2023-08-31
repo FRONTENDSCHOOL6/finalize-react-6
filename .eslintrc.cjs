eb5954521307bcaa15ab476b654ee20ac5fcbfd5
rustandbone <dlehdgh14@naver.com>
2023-08-31 22:39:42 +0900

    chore: 미사용 변수 알림 수준 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'warn',
+    'no-unused-vars': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
