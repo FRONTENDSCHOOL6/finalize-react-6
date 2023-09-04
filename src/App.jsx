4c9971ec3ffa4285425d2e2407fc9738340b7217
rustandbone <dlehdgh14@naver.com>
2023-09-05 01:11:15 +0900

    fix: lazy suspense 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Suspense } from 'react';
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,9 @@ function App() {
       <HelmetProvider>
         <QueryClientProvider client={queryClient}>
           <div className="App">
-            <RouterProvider router={router} />
+            <Suspense fallback="페이지 로딩 중...">
+              <RouterProvider router={router} />
+            </Suspense>
           </div>
           <ReactQueryDevtools />
         </QueryClientProvider>
