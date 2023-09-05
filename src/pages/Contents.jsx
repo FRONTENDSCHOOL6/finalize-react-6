143b99e4cb9a70633abab9bba455f014db104ed4
yxxung <loveus011@naver.com>
2023-09-05 18:39:08 +0900

    markup: 컨텐츠 페이지 버튼 컴포넌트 완성

--- src/pages/Contents.jsx
+++ src/pages/Contents.jsx
@@ -1,1 +1,38 @@
-export default function Contents() {}
+import { Helmet } from 'react-helmet-async';
+import right from '@/assets/right.svg';
+import logo from '@/assets/card1.png';
+
+export default function Contents() {
+  return (
+    <div className="flex flex-col justify-center">
+      <Helmet>
+        <title>나만의 제주 콘텐츠 페이지</title>
+      </Helmet>
+
+      <div className="text-center">
+        <h2 className="text-darkblue font-semibold text-4xl">혼자옵서예 !</h2>
+        <p className="mt-4">소중한 당신의 경험을 공유해 주세요.</p>
+
+        <button className="my-20 inline-flex rounded-full bg-lightsand gap-2 px-6 py-3 border border-blue">
+          <p className="text-blue font-bold text-lg">나만의 제주 등록하기</p>
+          <img src={right} alt="register" />
+        </button>
+      </div>
+
+      <div className="flex flex-wrap border h-[900px] mx-[10%]">
+        <div className="sm:basis-full lg:basis-1/3">
+          <img className="object-cover" src={logo} alt="" />
+        </div>
+        <div className="sm:basis-full lg:basis-1/3">
+          <img className="object-cover" src={logo} alt="" />
+        </div>
+        <div className='sm:basis-full lg:basis-1/3'>
+          <img className='object-cover' src={logo} alt="" />
+        </div>
+        <div className='sm:basis-full lg:basis-1/3'>
+          <img className='object-cover' src={logo} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
