4848be76f119f67fdaf330d2d9aaf558fd6486f2
haha41 <thgml500@gmail.com>
2023-09-05 17:47:07 +0900

    design: 헤더 width, h1 변경
    - width: 1920px 에서 full로 변경
    - h1 안에 img, span 삽입

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  
   return (
-    <header className="w-[1920px] h-[150px] flex flex-row items-center px-8 justify-between mx-auto">
-      <img src="/logo.png" alt="로고" className="w-[100px] h-[100px]" />
-      <h1 className="sr-only"> 당신의 제주를 우리의 제주로 </h1>
+    <header className="w-full h-[150px] flex flex-row items-center px-8 justify-between mx-auto">
+      <h1>
+        <img src="/logo.png" alt="로고" className="w-[100px] h-[100px]" />
+        <span className="sr-only">당신의 제주를 우리의 제주로 </span>
+      </h1>
       <ul className="flex flex-row items-center gap-10 text-[#717D96]">
         <li className="w-[150px] h-[50px] text-xl text-center leading-[50px] hover:border-b-[3px] hover:text-darkblue  hover:border-darkblue">
           <Link href="/">🍊 우리 제주</Link>
@@ -22,8 +23,12 @@ export default function Header() {
       </ul>
       <div className="flex flex-row items-center gap-5">
         {/* 로그인 하면 로그아웃, 내 프로필로 변경하기 */}
-        <button className="w-[100px] h-[100px] text-xl text-center leading-[100px]">로그인</button>
-        <button className="w-[100px] h-[100px] text-xl text-center leading-[100px]">회원가입</button>
+        <button className="w-[100px] h-[100px] text-xl text-center leading-[100px]">
+          로그인
+        </button>
+        <button className="w-[100px] h-[100px] text-xl text-center leading-[100px]">
+          회원가입
+        </button>
       </div>
     </header>
   );
