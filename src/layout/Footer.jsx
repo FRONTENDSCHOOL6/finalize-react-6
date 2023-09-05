67ad3020b34ed32feedb45de6de7cd8497a91cad
haha41 <thgml500@gmail.com>
2023-09-05 16:59:20 +0900

    markup: 푸터 마크업 완료

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="w-[1920px] h-[150px] flex flex-row items-center px-5 justify-center mx-auto bg-darkblue">
+      <p className="text-sand">
+      Copyright 2023 🥫 핫식스
+      </p>
+    </footer>
+  );
 }
