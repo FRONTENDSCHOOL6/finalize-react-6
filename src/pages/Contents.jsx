be58a22162b3e707f03b38f4ee72b3f279f3f638
yxxung <loveus011@naver.com>
2023-09-05 20:02:17 +0900

    markup: 메인페이지 마크업 완료

--- src/pages/Contents.jsx
+++ src/pages/Contents.jsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 import right from '@/assets/right.svg';
 import logo from '@/assets/card1.png';
+import { getPbImageURL } from '@/utils';
+import ContentItem from '@/components/content/ContentItem';
 
 export default function Contents() {
   return (
@@ -9,30 +11,29 @@ export default function Contents() {
         <title>나만의 제주 콘텐츠 페이지</title>
       </Helmet>
 
-      <div className="text-center">
-        <h2 className="text-darkblue font-semibold text-4xl">혼자옵서예 !</h2>
+      <section className="text-center mt-20">
+        <h2 className="text-darkblue font-semibold text-4xl">나만의 제주</h2>
         <p className="mt-4">소중한 당신의 경험을 공유해 주세요.</p>
 
-        <button className="my-20 inline-flex rounded-full bg-lightsand gap-2 px-6 py-3 border border-blue">
-          <p className="text-blue font-bold text-lg">나만의 제주 등록하기</p>
+        <button className="my-20 inline-flex rounded-full bg-lightsand gap-2 px-6 py-3 border-2 border-blue">
+          <p className="text-blue font-extrabold text-lg">추억을 공유하기</p>
           <img src={right} alt="register" />
         </button>
-      </div>
+      </section>
 
-      <div className="flex flex-wrap border h-[900px] mx-[10%]">
-        <div className="sm:basis-full lg:basis-1/3">
-          <img className="object-cover" src={logo} alt="" />
-        </div>
-        <div className="sm:basis-full lg:basis-1/3">
-          <img className="object-cover" src={logo} alt="" />
-        </div>
-        <div className='sm:basis-full lg:basis-1/3'>
-          <img className='object-cover' src={logo} alt="" />
-        </div>
-        <div className='sm:basis-full lg:basis-1/3'>
-          <img className='object-cover' src={logo} alt="" />
-        </div>
-      </div>
+      <section className="grid lg:grid-flow-row lg:grid-cols-3 p-1 gap-1 lg:max-h-[900px] mx-[10%] bg-gray-100">
+        <ContentItem />
+        <ContentItem />
+        <ContentItem />
+        <ContentItem />
+      </section>
+
+      <section className='flex justify-center my-20'>
+        <button type="button" className='px-4 text-gray-400 text-xl'>&lt;</button>
+        {/* 추후 api 통신하면서 수정 */}
+        <div>1 2 3</div>
+        <button type="button" className='px-4 text-gray-400 text-xl'>&gt;</button>
+      </section>
     </div>
   );
 }
