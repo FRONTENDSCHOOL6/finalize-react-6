24cc28f821dd85a482e793d2e88d5f374e487846
haha41 <thgml500@gmail.com>
2023-09-05 17:54:17 +0900

    [chore] tailwind.config.js 수정
    - screens 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */;
 export default {
-  content: ["./index.html", "./src/**/*.jsx"],
+  content: ['./index.html', './src//*.jsx'],
   theme: {
     extend: {
       fontFamily: {
-        suit: ["SUIT Variable"],
+        suit: ['SUIT Variable'],
       },
       colors: {
-        darkblue: "#013F4E",
-        blue: "#00718F",
-        lightblue: "#0BA5BE",
-        sand: "#CFCBC8",
-        green: "#576319",
+        darkblue: '#013F4E',
+        blue: '#00718F',
+        lightblue: '#0BA5BE',
+        sand: '#CFCBC8',
+        green: '#576319',
+        lightsand: '#EDF0F7',
+      },
+      screens: {
+        sm: { min: '320px', max: '819px' },
+        lg: { min: '820px' },
       },
     },
   },
