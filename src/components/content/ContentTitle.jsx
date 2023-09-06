05ba77c286039c6cdf9be9c42e6748884bbc2dfa
yxxung <loveus011@naver.com>
2023-09-06 12:17:19 +0900

    design: title css 수정

--- src/components/content/ContentTitle.jsx
+++ src/components/content/ContentTitle.jsx
@@ -1,6 +1,6 @@
 export default function ContentTitle({title}) {
   return (
-    <div className="flex items-center justify-center bg-blue mx-[30%] h-12 mt-10 rounded-md">
+    <div className="flex py-2 items-center justify-center border-2 border-lightsand bg-blue mx-[30%] h-16 mt-10 rounded-md">
       <h2 className="text-white text-3xl">{title}</h2>
     </div>
   );
