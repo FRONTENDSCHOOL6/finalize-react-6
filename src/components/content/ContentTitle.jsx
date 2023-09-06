2f6f3d8124b2b949daf8033e7b3561d4fc3c5043
JiYoung Park <74893676+yxxung@users.noreply.github.com>
2023-09-06 18:16:43 +0900

    Revert "헤더, 날씨 페이지 컴포넌트 분리 했습니다."

--- src/components/content/ContentTitle.jsx
+++
@@ -1,7 +0,0 @@
-export default function ContentTitle({title}) {
-  return (
-    <div className="flex py-2 items-center justify-center border-2 border-lightsand bg-blue mx-[30%] h-16 mt-10 rounded-md">
-      <h2 className="text-white text-3xl">{title}</h2>
-    </div>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,11 @@
-/ @type {import('tailwindcss').Config} */;
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.jsx'],
+  content: ["./index.html", "./src/**/*.jsx"],
   theme: {
     extend: {
       fontFamily: {
-        suit: ['SUIT Variable'],
+        suit: ["SUIT Variable"],
       },
-      colors: {
-        darkblue: '#013F4E',
-        blue: '#00718F',
-        lightblue: '#0BA5BE',
-        sand: '#CFCBC8',
-        green: '#576319',
-        lightsand: '#EDF0F7',
-        KakaoYellow: '#FEE500',
-      },
-      screens: {
-        sm: { min: '320px', max: '999px' },
-        lg: { min: '1000px' },
-      },
-      boxShadow: {
-        content: 'rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px',
-        comment: 'rgba(27, 31, 35, 0.07) 0px 1px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px inset'
-      }
     },
   },
   plugins: [],
