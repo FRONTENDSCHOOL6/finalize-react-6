30937fd41250b7a5a923eccd4152062408df9f55
haha41 <thgml500@gmail.com>
2023-09-07 00:56:06 +0900

    markup: 교통 정보 페이지 마크업 완료
    related to: #13

---
+++ src/components/GetKakaoMap.jsx
@@ -0,0 +1,10 @@
+export default function GetKakaoMap() {
+  return (
+    <>
+      카카오맵 api<br></br>
+      [가이드] https://apis.map.kakao.com/web/guide/#more<br></br>
+      [지도 생성] https://apis.map.kakao.com/web/sample/basicMap/<br></br>
+      [교통정보 표시] https://apis.map.kakao.com/web/sample/addTrafficOverlay/
+    </>
+  );
+}
--- src/pages/TrafficInfo.jsx
+++ src/pages/TrafficInfo.jsx
@@ -1,1 +1,18 @@
-export default function TrafficInfo() {}
+import GetKakaoMap from '@/components/GetKakaoMap';
+import { Helmet } from 'react-helmet-async';
+
+export default function TrafficInfo() {
+  return (
+    <div className="px-8 my-10 flex flex-col justify-center items-center">
+      <Helmet>
+        <title>Jeju All in One - 제주 교통</title>
+      </Helmet>
+      <h2 className="text-darkblue font-semibold text-4xl text-center my-10">
+        실시간 교통 상황
+      </h2>
+      <div className="border-2 w-5/6">
+        <GetKakaoMap />
+      </div>
+    </div>
+  );
+}
