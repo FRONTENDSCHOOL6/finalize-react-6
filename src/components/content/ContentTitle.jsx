dbe54ecd22450a6ef11ffdd3139e8d66c159b3fd
rustandbone <dlehdgh14@naver.com>
2023-09-07 17:19:18 +0900

    style: 콘텐츠 제목 마진 수정

--- src/components/content/ContentTitle.jsx
+++ src/components/content/ContentTitle.jsx
@@ -1,13 +1,13 @@
-import { string } from "prop-types";
+import { string } from 'prop-types';
 
-export default function ContentTitle({title}) {
+export default function ContentTitle({ title }) {
   return (
-    <div className="flex py-2 items-center justify-center mt-10">
+    <div className="flex py-2 items-center justify-center mt-5">
       <h2 className="text-blue text-4xl font-semibold">{title}</h2>
     </div>
   );
 }
 
 ContentTitle.propTypes = {
-  title: string.isRequired
-}
+  title: string.isRequired,
+};
