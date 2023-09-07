1ebde65c31ba9572a75f2ac075137615db49cc23
haha41 <thgml500@gmail.com>
2023-09-07 16:32:27 +0900

    feat: 날씨 지역 선택 기능 부분 구현
    - 1차 지역 선택 후 2차 지역 선택 가능함
    - 지역 선택 하면 좌표와 연결됨
    - 좌표를 통해 데이터 가져오는 것은 구현 할 예정
    
    related to: #34

--- src/components/weather/SelectLocation.jsx
+++ src/components/weather/SelectLocation.jsx
@@ -1,17 +1,55 @@
+import { useState } from 'react';
+import data from '@/data.json';
+
 export default function SelectLocation() {
+  const [city, setCity] = useState('');
+  const [sublocation, setSublocation] = useState('');
+  const [coordinates, setCoordinates] = useState({ x: null, y: null });
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+    setSublocation(''); // city가 변경되면 sublocation 초기화
+    setCoordinates({ x: null, y: null }); // 좌표도 초기화
+  };
+
+  const handleSublocationChange = (e) => {
+    setSublocation(e.target.value);
+
+    // 해당 sublocation의 좌표 찾기
+    const locationData = data[city].find((loc) => loc.name === e.target.value);
+
+    if (locationData) {
+      setCoordinates({ x: locationData.x, y: locationData.y });
+    }
+  };
+
   return (
     <div className="h-[50px] leading-[50px]">
       <span className="w-[100px] inline-flex text-xl">지역</span>
-      <select name="location" className="mx-3">
+      <select name="city" className="mx-3" onChange={handleCityChange}>
         <option value="">- 선택 -</option>
-        <option value="제주시">제주시</option>
-        <option value="서귀포시">서귀포시</option>
+        {Object.keys(data).map((cityName) => (
+          <option key={cityName} value={cityName}>
+            {cityName}
+          </option>
+        ))}
       </select>
-      <select>
-        <option>- 선택 -</option>
-        <option>OO동</option>
-        <option>OO동</option>
+
+      <select name="sublocation" onChange={handleSublocationChange}>
+        <option value="">- 선택 -</option>
+
+        {(data[city] || []).map((loc) => (
+          <option key={loc.name} value={loc.name}>
+            {loc.name}
+          </option>
+        ))}
       </select>
+
+      {coordinates.x && coordinates.y && (
+        <p>
+          선택된 지역의 좌표는 ({coordinates.x}, {coordinates.y})입니다.
+        </p>
+      )}
     </div>
   );
 }
