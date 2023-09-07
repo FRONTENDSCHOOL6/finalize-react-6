d523d75d2e1ee474725d03a0556ac38d30d4da40
yxxung <loveus011@naver.com>
2023-09-07 17:52:07 +0900

    feat: 로그인 기능 구현
    related to: #44

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,62 @@
-import InputField from "@/components/InputField";
-import Logo from "@/components/Logo";
-import Button from "@/components/Button";
-import LinkItem from "@/components/LinkItem";
-import { Helmet } from "react-helmet-async";
+import pb from '@/api/pocketbase';
+import Button from '@/components/Button';
+import InputField from '@/components/InputField';
+import LinkItem from '@/components/LinkItem';
+import Logo from '@/components/Logo';
+import KakaoLogin from '@/components/login/KakaoLogin';
+import debounce from '@/utils/debounce';
+import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const { state } = useLocation(); // location { pathname, search, hash, state = {  } }
+
+  const [formState, setFormState] = useState({
+    userId: '',
+    password: '',
+  });
+
+  const [isLogin, setIsLogin] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { userId, password } = formState;
+
+    try {
+      const response = await pb
+        .collection('user')
+        .authWithPassword(userId, password);
+
+      setIsLogin(true);
+      // 자동으로 localstorage에 저장
+      // const { nickname, email } = response.record;
+      // const userToken = response.token;
+
+      // sessionStorage.setItem('userToken', userToken);
+
+      if (!state) {
+        navigate('/');
+      } else {
+        const { wishLocationPath } = state;
+        navigate(wishLocationPath === '/login' ? '/' : wishLocationPath, { replace: true });
+      }
+    } catch (error) {
+      setIsLogin(false);
+      console.log('로그인 실패\n', error);
+    }
+  };
+
+  const handleInput = debounce((e) => {
+    const { name, value } = e.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  }, 400);
+
   return (
     <>
       {/* 헤드 이름 */}
@@ -15,13 +67,32 @@ export default function Login() {
       {/* 마크업 */}
       <div className="flex flex-col items-center justify-center my-20">
         <Logo />
-        <form className="flex flex-col gap-3">
-          <InputField id="id" type="text" placeholder="아이디" />
-          <InputField id="password" type="password" placeholder="비밀번호" />
-          <Button>로그인</Button>
-          <a href="/">
-            <Button txtColor="black" bgColor="KakaoYellow">카카오로 로그인</Button>
-          </a>
+        <form className="flex flex-col gap-3" onSubmit={handleLogin}>
+          <InputField
+            name="userId"
+            type="text"
+            placeholder="아이디"
+            defaultValue={formState.userId}
+            onChange={handleInput}
+          />
+          <InputField
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            defaultValue={formState.password}
+            onChange={handleInput}
+          />
+
+          {isLogin === false ? (
+            <div className="text-center text-red-600 my-2">
+              아이디 혹은 비밀번호를 잘못 입력했습니다.
+            </div>
+          ) : (
+            ''
+          )}
+
+          <Button type="submit" txtColor="white" bgColor="bg-blue">로그인</Button>
+          <KakaoLogin />
         </form>
         <div className="m-8">
           <LinkItem link="/findid">아이디 찾기</LinkItem>
@@ -30,11 +101,11 @@ export default function Login() {
         </div>
         <p>
           아직 회원이 아니신가요?&nbsp;
-          <LinkItem link="/join" className="font-extrabold text-blue">회원가입 하기</LinkItem>
+          <LinkItem link="/join" className="font-extrabold text-blue">
+            회원가입 하기
+          </LinkItem>
         </p>
       </div>
     </>
-  )
+  );
 }
-
-
