fbb15f4e649b1a7c57f6993f95da198b6e74a743
yxxung <loveus011@naver.com>
2023-09-07 11:14:24 +0900

    react: 페이지 별 helmet 추가

--- src/components/content/ContentTitle.jsx
+++ src/components/content/ContentTitle.jsx
@@ -1,7 +1,13 @@
+import { string } from "prop-types";
+
 export default function ContentTitle({title}) {
   return (
-    <div className="flex py-2 items-center justify-center border-2 border-lightsand bg-blue mx-[30%] h-16 mt-10 rounded-md">
-      <h2 className="text-white text-3xl">{title}</h2>
+    <div className="flex py-2 items-center justify-center mt-10">
+      <h2 className="text-blue text-4xl font-semibold">{title}</h2>
     </div>
   );
+}
+
+ContentTitle.propTypes = {
+  title: string.isRequired
 }
