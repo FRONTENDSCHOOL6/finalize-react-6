fc1e15a4a7291ac1ee656930cfd9f86e3e76ce8d
miinje <njjj00083@gmail.com>
2023-09-08 16:05:17 +0900

    feat: 카카오 맵 api
    related to: #54

--- src/pages/TrafficInfo.jsx
+++ src/pages/TrafficInfo.jsx
@@ -1,18 +1,20 @@
 import GetKakaoMap from '@/components/GetKakaoMap';
+import PageHead from '@/components/PageHead';
 import { Helmet } from 'react-helmet-async';
 
 export default function TrafficInfo() {
   return (
-    <div className="px-8 my-10 flex flex-col justify-center items-center">
-      <Helmet>
-        <title>Jeju All in One - 제주 교통</title>
-      </Helmet>
-      <h2 className="text-darkblue font-semibold text-4xl text-center my-10">
-        실시간 교통 상황
-      </h2>
-      <div className="border-2 w-5/6">
-        <GetKakaoMap />
+    <>
+      <PageHead title="Jeju All in One - 제주 교통" />
+
+      <div className="px-8 my-10 flex flex-col justify-center items-center">
+        <h2 className="text-darkblue font-semibold text-4xl text-center my-10">
+          실시간 교통 상황
+        </h2>
+        <div className="flex items-center border-4 border-blue w-5/6 h-[900px]">
+          <GetKakaoMap />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
