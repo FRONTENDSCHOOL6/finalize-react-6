e81e80613673eb4946aa8ba6f71221dd38e4f35e
yxxung <loveus011@naver.com>
2023-09-08 14:06:10 +0900

    design: cursor pointer로 변경

--- src/components/header/NavigationSide.jsx
+++ src/components/header/NavigationSide.jsx
@@ -4,7 +4,7 @@ export default function NavigationSide({ menu, onClick }) {
   return (
     <li
       onClick={onClick}
-      className="text-base text-center font-semibold leading-8 text-darkblue hover:border-b-[3px] hover:border-darkblue"
+      className="text-base text-center font-semibold leading-8 text-darkblue hover:border-b-[3px] hover:border-darkblue cursor-pointer"
     >
       {menu}
     </li>
