833f5cab0266da3ef5891384197e03a3d5867577
haha41 <thgml500@gmail.com>
2023-09-09 02:24:24 +0900

    feat: 3일치 날씨 가져오기 성공
    related to: #36

--- src/components/weather/WeatherTable2.jsx
+++ src/components/weather/WeatherTable2.jsx
@@ -3,32 +3,41 @@ import OnedayWeather2 from './OnedayWeather2';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-export default function WeatherTable2({
-  coordinates = { x: 52, y: 38 },
-  baseDate,
-}) {
+export default function WeatherTable2({ coordinates }) {
   const [data, setData] = useState({});
-
-  const baseUrl =
-    'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
-  const serviceKey = import.meta.env.VITE_WEATHER_API_KEY;
-
   useEffect(() => {
+    const baseUrl =
+      'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst';
+    const serviceKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+    // 날짜
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    let baseDate = `${year}${month}${day}`;
+    const hour = today.getHours();
+    if (hour < 2) {
+      // 새벽 2시 이전인 경우 전날 23시로 baseTime 설정
+      const yesterday = new Date(today);
+      yesterday.setDate(yesterday.getDate() - 1);
+      const prevDayYear = yesterday.getFullYear();
+      const prevDayMonth = String(yesterday.getMonth() + 1).padStart(2, '0');
+      const prevDayDate = String(yesterday.getDate()).padStart(2, '0');
+
+      baseDate = `${prevDayYear}${prevDayMonth}${prevDayDate}`;
+    }
+
     async function fetchWeatherData() {
       const { x, y } = coordinates;
       const url = `${baseUrl}?serviceKey=${serviceKey}&pageNo=1&numOfRows=1500&dataType=JSON&base_date=${baseDate}&base_time=0200&nx=${x}&ny=${y}`;
       try {
         const response = await fetch(url);
-        console.log(url);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const result = await response.json();
-        console.log('setData 전 data:', data);
-        console.log('setData 전 result:', result);
         setData(result);
-        console.log('setData 후 data:', data);
-        console.log('setData 후 result:', result);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -37,63 +46,77 @@ export default function WeatherTable2({
   }, [coordinates]);
 
   return (
-    <table className="border-2 w-2/3 text-center text-xl">
-      <thead className="border-2">
-        <tr className="bg-lightsand">
-          <th className="border-2 h-[50px]"></th>
-          {data?.response?.body?.items?.item
-            .filter((item) => {
-              if (item.fcstDate && item.category === 'TMX') {
-                return true; // 첫 번째로 발견된 해당 날짜의 예보만 선택
-              }
-              return false; // 코드의 의도를 명확히 하기 위해 작성
-            })
-
-            .slice(0, 3) // 3일(24시간*3칸) 랜더링
-            .map((item) => {
-              const month = parseInt(item.fcstDate.slice(4, 6), 10); // '09' -> 9
-              const day = parseInt(item.fcstDate.slice(6), 10); // '01' -> 1
-              return (
-                <th
-                  key={`${item.fcstDate}-${item.fcstTime}-${item.category}`}
-                  className="border-2"
-                >
-                  {`${month}/${day}`}
-                </th>
-              );
-            })}
-        </tr>
-      </thead>
-      <tbody className="border-2">
-        <tr>
-          <th className="border-2 h-[50px]">
-            최<span className="text-red-600">고</span> 기온
-          </th>
-          {/* {data && <OnedayWeather2 data={data} category={'TMX'} text={'°C'} />} */}
-        </tr>
-        <tr>
-          <th className="border-2 h-[50px]">
-            최<span className="text-sky-600">저</span> 기온
-          </th>
-          {/* <OnedayWeather2 data={data} category={'TMN'} text={'°C'} /> */}
-        </tr>
-        <tr className="bg-lightsand">
-          <th className="border-2 h-[50px]">하늘 상태</th>
-          {/* <OnedayWeather2 data={data} category={'SKY'} /> */}
-        </tr>
-        <tr>
-          <th className="border-2 h-[50px]">강수 형태</th>
-          {/* <OnedayWeather2 data={data} category={'PTY'} /> */}
-        </tr>
-        <tr>
-          <th className="border-2 h-[50px] bg-lightsand">풍속</th>
-          {/* <OnedayWeather2 data={data} category={'WSD'} text={'%'} /> */}
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table className="border-2 w-2/3 text-center text-xl">
+        <thead className="border-2">
+          <tr className="bg-lightsand">
+            <th className="border-2 h-[50px]"></th>
+            {data?.response?.body?.items?.item
+              .filter((item) => {
+                if (item.fcstDate && item.category === 'TMX') {
+                  return true;
+                }
+                return false;
+              })
+              .map((item) => {
+                const month = parseInt(item.fcstDate.slice(4, 6), 10);
+                const day = parseInt(item.fcstDate.slice(6), 10);
+                return (
+                  <th
+                    key={`${item.fcstDate}-${item.fcstTime}-${item.category}`}
+                    className="border-2"
+                  >
+                    {`${month}/${day}`}
+                  </th>
+                );
+              })}
+          </tr>
+        </thead>
+        <tbody className="border-2">
+          <tr>
+            <th className="border-2 h-[50px]">
+              최<span className="text-red-600">고</span> 기온
+            </th>
+            {data && (
+              <OnedayWeather2 data={data} category={'TMX'} text={'°C'} />
+            )}
+          </tr>
+          <tr>
+            <th className="border-2 h-[50px]">
+              최<span className="text-sky-600">저</span> 기온
+            </th>
+            <OnedayWeather2 data={data} category={'TMN'} text={'°C'} />
+          </tr>
+          <tr className="bg-lightsand">
+            <th className="border-2 h-[50px]">하늘 상태</th>
+            <OnedayWeather2 data={data} category={'SKY'} />
+          </tr>
+          <tr>
+            <th className="border-2 h-[50px]">습도</th>
+            <OnedayWeather2 data={data} category={'REH'} text={'%'} />
+          </tr>
+          <tr className=" bg-lightsand">
+            <th className="border-2 h-[50px]">강수 형태</th>
+            <OnedayWeather2 data={data} category={'PTY'} />
+          </tr>
+          <tr>
+            <th className="border-2 h-[50px]">강수 확률</th>
+            <OnedayWeather2 data={data} category={'POP'} text={'%'} />
+          </tr>
+          <tr className="bg-lightsand">
+            <th className="border-2 h-[50px]">풍속</th>
+            <OnedayWeather2 data={data} category={'WSD'} text={'m/s'} />
+          </tr>
+        </tbody>
+      </table>
+      <span className="w-2/3 text-right mt-[-30px]">
+        풍속: 4미만(약함), 4이상 9미만(약간 강함), 9이상 14미만(강함),
+        14이상(매우강함)
+      </span>
+    </>
   );
 }
 
 WeatherTable2.propTypes = {
-  // data: PropTypes.object.isRequired,
+  coordinates: PropTypes.object.isRequired,
 };
--- src/pages/WeatherInfo.jsx
+++ src/pages/WeatherInfo.jsx
@@ -1,14 +1,13 @@
 import SelectLocation from '@/components/weather/SelectLocation';
 import { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet-async';
 import GetWeather from '@/components/weather/GetWeather';
 import WeatherTable2 from '@/components/weather/WeatherTable2';
+import PageHead from '@/components/PageHead';
 
 export default function WeatherInfo() {
   const [data, setData] = useState({});
   const [coordinates, setCoordinates] = useState({ x: 52, y: 38 });
   const [sublocation, setSublocation] = useState('');
-  const [baseDate, setBaseDate] = useState(''); // baseDate 상태 추가
 
   useEffect(() => {
     const baseUrl =
@@ -37,7 +36,7 @@ export default function WeatherInfo() {
     let baseTime;
 
     if (hour < 2) {
-      // 자정 이전인 경우 전날의 시간으로 설정
+      // 새벽 2시 이전인 경우 전날 23시로 baseTime 설정
       const yesterday = new Date(today);
       yesterday.setDate(yesterday.getDate() - 1);
       const prevDayYear = yesterday.getFullYear();
@@ -48,27 +47,21 @@ export default function WeatherInfo() {
 
       baseTime = '2300';
     } else {
-      // 현재 시간 기준으로 가장 가까운 시간 찾기
+      // 현재 시간 기준으로 가장 가까운 baseTime 찾기
       const index = baseTimes.findIndex(
         (time) => hour < parseInt(time.slice(0, 2))
       );
       baseTime = index !== -1 ? baseTimes[index - 1] : '2300';
     }
-
     async function fetchWeatherData() {
       const url = `${baseUrl}?serviceKey=${serviceKey}&pageNo=1&numOfRows=1500&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=${coordinates.x}&ny=${coordinates.y}`;
       try {
         const response = await fetch(url);
-        // console.log(url);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
         setData(data);
-        setBaseDate(baseDate); // baseDate 상태 업데이트
-
-        // setData({ ...data, baseDate });
-        // console.log(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -78,23 +71,22 @@ export default function WeatherInfo() {
   }, [coordinates]);
 
   return (
-    <div className="px-8 my-10">
-      <Helmet>
-        <title>Jeju All in One - 제주 날씨</title>
-      </Helmet>
-      <h2 className="text-darkblue font-semibold text-4xl text-center my-10">
-        제주 날씨
-        {sublocation && `(${sublocation})`}
-      </h2>
-      <div className="flex flex-col justify-center items-center gap-10">
-        <SelectLocation
-          onCoordinatesChange={setCoordinates}
-          onSublocationChange={setSublocation}
-        />
-        <GetWeather data={data} />
-        {/* <WeatherTable2 data={data} baseDate={baseDate}/> */}
-        <WeatherTable2 coordinates={coordinates} baseDate={baseDate} />
+    <>
+      <PageHead title={'Jeju All in One - 제주 날씨'} />
+      <div className="px-8 my-10">
+        <h2 className="text-darkblue font-semibold text-4xl text-center my-10">
+          제주 날씨
+          {sublocation && `(${sublocation})`}
+        </h2>
+        <div className="flex flex-col justify-center items-center gap-10">
+          <SelectLocation
+            onCoordinatesChange={setCoordinates}
+            onSublocationChange={setSublocation}
+          />
+          <GetWeather data={data} />
+          <WeatherTable2 coordinates={coordinates} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
