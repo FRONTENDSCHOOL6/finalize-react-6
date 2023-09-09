42b2e6ac6741e2de477f11b6ce041647f3d8834b
haha41 <thgml500@gmail.com>
2023-09-10 08:23:17 +0900

    feat: 메인 페이지 댓글 많은 순으로 3개씩 최대 9개 랜더링 되도록 구현
    related to: #40

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -7,7 +7,7 @@ export default function MainContent({ src, alt, title }) {
   const [data, setData] = useState(null);
   const [status, setStatus] = useState('pending');
   const [page, setPage] = useState(1); // 현재 페이지 번호
-  const [totalItems, setTotalItems] = useState(0);
+  // const [totalItems, setTotalItems] = useState(0);
 
   useEffect(() => {
     async function getContentList() {
@@ -22,17 +22,28 @@ export default function MainContent({ src, alt, title }) {
           (item) => item.commentId.length > 0
         );
 
-        // 댓글 수에 따라 내림차순으로 정렬
-        const sortedList = commentList.sort(
-          (a, b) => b.commentId.length - a.commentId.length
-        );
+        // 댓글 수에 따라 내림차순으로 정렬, 그리고 동일한 댓글 수의 경우 최신 순으로 정렬
+        const sortedList = commentList.sort((a, b) => {
+          if (b.commentId.length !== a.commentId.length) {
+            return b.commentId.length - a.commentId.length;
+          } else {
+            return new Date(b.created) - new Date(a.created);
+          }
+        });
 
         // 처음 9개 요소만 선택
         const nineItems = sortedList.slice(0, 9);
 
-        console.log('nineItems:', nineItems);
+        // 전체 아이템 목록에서 현재 페이지에 해당하는 부분만 추출
+        const itemsPerPage = 3;
+        const startIndex = (page - 1) * itemsPerPage; // 현재 페이지의 첫 번째 아이템 인덱스
+        const endIndex = startIndex + itemsPerPage; // 현재 페이지의 마지막 아이템 인덱스
+
+        const currentPageItems = nineItems.slice(startIndex, endIndex);
+
+        console.log('currentPageItems:', currentPageItems);
         // setData(contentList);
-        setData(nineItems);
+        setData(currentPageItems);
         setStatus('success');
         // setTotalItems(contentList.totalItems); // API 응답에서 total 값 업데이트
       } catch (error) {
@@ -63,7 +74,7 @@ export default function MainContent({ src, alt, title }) {
       ))}
 
       {/* Pagination Controls */}
-      {/* <button
+      <button
         onClick={() => setPage((prevPage) => prevPage - 1)}
         disabled={page === 1}
       >
@@ -74,21 +85,10 @@ export default function MainContent({ src, alt, title }) {
 
       <button
         onClick={() => setPage((prevPage) => prevPage + 1)}
-        disabled={page >= Math.ceil(totalItems / 3)}
+        disabled={page === 3}
       >
         Next Page
-      </button> */}
-
-      {/* <li className="w-1/3 border-2 border-slate-300 border-solid rounded">
-        <NavLink to="/content">
-          <figure>
-            <img src={src} alt={alt} />
-            <figcaption className="py-4 pl-4 overflow-hidden text-ellipsis whitespace-nowrap">
-              {title}
-            </figcaption>
-          </figure>
-        </NavLink>
-      </li> */}
+      </button>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,12 +24,6 @@ export default function Home() {
             alt="메밀꽃밭"
             title="제주 메밀꽃"
           />
-          <MainContent src="/jejuImage4.jpg" alt="돌담" title="제주 돌담" />
-          <MainContent
-            src="/jejuImage3.jpg"
-            alt="한라봉 과수원"
-            title="제주는 한라봉이면 충분하다고 할 수 있지 않을까?"
-          />
         </ul>
         <button className="flex mx-auto mt-10 rounded-full bg-lightsand px-6 py-3 border-2 border-blue">
           <Link to="content/create">
