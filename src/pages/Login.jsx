1e7a251c956f23b49f39b69cddcc1102761c07aa
yxxung <loveus011@naver.com>
2023-09-12 09:32:38 +0900

    react: 컴포넌트 위치 변경

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import pb from '@/api/pocketbase';
 import Button from '@/components/Button';
 import InputField from '@/components/InputField';
 import LinkItem from '@/components/LinkItem';
-import LoginPageContent from '@/components/LoginPageContent';
+import LoginPageContent from '@/components/login/LoginPageContent';
 import Logo from '@/components/Logo';
 import PageHead from '@/components/PageHead';
 import KakaoLogin from '@/components/login/KakaoLogin';
