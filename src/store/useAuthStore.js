c3e032295744b0921e108ed9db811ba6ae326d9c
rustandbone <dlehdgh14@naver.com>
2023-09-14 09:55:18 +0900

    react: 로그인 시 storage 올라가는 정보에 id 추가

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,38 +3,45 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 const KAKAO_CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
-const KAKAO_LOGOUT_REDIRECT_URI = import.meta.env.VITE_KAKAO_LOGOUT_REDIRECT_URI;
+const KAKAO_LOGOUT_REDIRECT_URI = import.meta.env
+  .VITE_KAKAO_LOGOUT_REDIRECT_URI;
 
 const userInitState = {
   username: null,
   userId: null,
   token: null,
   email: null,
-  isKakao: false
+  id: null,
+  isKakao: false,
 };
 
-export const useAuthStore = create(persist((set, get) => ({
-  user: userInitState,
-
-  setUser: (user) => {
-    set({ user: user });
-  },
-
-  logout: () => {
-    const isKakao = get().user.isKakao;
-
-    set({
+export const useAuthStore = create(
+  persist(
+    (set, get) => ({
       user: userInitState,
-    });
 
-    if (isKakao === true) {
-      location.replace(
-        `https://kauth.kakao.com/oauth/logout?client_id=${KAKAO_CLIENT_ID}&logout_redirect_uri=${KAKAO_LOGOUT_REDIRECT_URI}`
-      );
-    } else if(isKakao === false) {
-      pb.authStore.clear();
+      setUser: (user) => {
+        set({ user: user });
+      },
+
+      logout: () => {
+        const isKakao = get().user.isKakao;
+
+        set({
+          user: userInitState,
+        });
+
+        if (isKakao === true) {
+          location.replace(
+            `https://kauth.kakao.com/oauth/logout?client_id=${KAKAO_CLIENT_ID}&logout_redirect_uri=${KAKAO_LOGOUT_REDIRECT_URI}`
+          );
+        } else if (isKakao === false) {
+          pb.authStore.clear();
+        }
+      },
+    }),
+    {
+      name: 'user',
     }
-  },
-}), {
-  name: 'user'
-}));
+  )
+);
