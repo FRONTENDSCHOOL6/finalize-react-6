51a02ad11a5c7ab8909f1351da72ec68feff2538
miinje <njjj00083@gmail.com>
2023-09-14 14:05:20 +0900

    feat: 회원가입 유효성 검사 및 중복 확인 기능 - input 채워지지 않을 시 각각 칸에 맞는 toast.error 띄우기 - 아이디 / 비밀번호 / 이메일 형식 오류 시 toast.error 띄우기 - 아이디 / 이메일 / 닉네임 중복 시 toast.error 띄우기 - 회원가입 실패 시 console과 toast 에러 문구 띄우기 - validation.js 이메일 형식 검증 정규식 추가 related to: #46

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -6,6 +6,14 @@ export function idReg(text){
 
 //특수문자포함 최소 8자이상~16자이하 비밀번호정규식
 export function pwReg(text){
-  const reg = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-]).{8,16}$/;
+  const reg = /^(?=.*[a-zA-Z])(?=.*[,~!@#$%^*+=-]).{8,16}$/;
   return reg.test(String(text).toLowerCase());
+}
+
+//이메일 형식 검증 정규식
+export function emailReg(text) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
 }
