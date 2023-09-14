ed93ce802fff0a23b6272ed4e38220273050294e
yxxung <loveus011@naver.com>
2023-09-14 14:52:40 +0900

    refactor: Navigation component 리팩토링

--- src/components/header/NavigationCenter.jsx
+++
@@ -1,9 +0,0 @@
-export default function NavigationCenter({ menu }) {
-  return (
-    <>
-      <li className="text-base text-center leading-8 hover:border-b-[3px] hover:text-darkblue  hover:border-darkblue">
-        {menu}
-      </li>
-    </>
-  );
-}
