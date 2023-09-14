e915b3aa83a6db80039c8159b1369a5d20724cb7
yxxung <loveus011@naver.com>
2023-09-14 15:15:51 +0900

    fix: 헤더 상단 고정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -27,15 +27,15 @@ export default function Header() {
   };
 
   return (
-    <header className={`${mainHeader} z-10 max-w-full mx-auto`}>
-      <nav className="flex items-center justify-between space-x-4 px-8">
+    <header className={`${mainHeader} fixed z-10 h-20 w-full mx-auto`}>
+      <nav className="flex items-center h-20 justify-between space-x-4 px-8">
         <h1>
           <NavLink to="/" className="flex items-center">
             <img src="/logo.png" alt="로고" className="w-24" />
             <span className="sr-only">Jeju - All in One</span>
           </NavLink>
         </h1>
-        <ul className="hidden mobile:flex items-center px-4 mx-auto font-semibold font-heading space-x-12 text-gray-600">
+        <ul className="hidden mobile:flex mobile:items-center px-4 mx-auto font-semibold font-heading space-x-12 text-gray-600">
           <NavLink
             to="content/list"
             style={isActive}
@@ -117,7 +117,7 @@ export default function Header() {
         </div>
       </nav>
       <div className={classNames('mobile:hidden', { hidden: !menuToggle })}>
-        <ul className="mobile:flex justify-center bg-slate-100 py-2 font-semibold font-heading list-none text-gray-600">
+        <ul className={`${mainHeader} mobile:flex justify-center bg-slate-100 py-2 font-semibold font-heading list-none text-gray-600`}>
           <li className={`text-center py-2 ${user.token ? 'block' : 'hidden'}`}>{user.username} 님</li>
           <NavLink
             to="content/list"
