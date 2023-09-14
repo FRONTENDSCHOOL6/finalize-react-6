dab60bdda99d1e937b3fabf89456bfc5583ea011
rustandbone <dlehdgh14@naver.com>
2023-09-14 09:56:51 +0900

    react: 프로필 페이지 로그인 유저 정보 가져오기
    related to: #42

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,31 +1,46 @@
+import pb from '@/api/pocketbase';
 import MainContent from '@/components/MainContent';
 import ProfileComment from '@/components/ProfileComment';
 import TitleButton from '@/components/TitleButton';
+import { useAuthStore } from '@/store/useAuthStore';
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 export default function MyProfile() {
+  const { user } = useAuthStore();
+
+  useEffect(() => {
+    async function getContent() {
+      try {
+        const jejuContent = await pb
+          .collection('user')
+          .getOne(
+            user.id,
+            { expand: 'comment,content' },
+            { requestKey: 'string' }
+          );
+
+        console.log(jejuContent);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getContent();
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Jeju All in One - 내 프로필</title>
       </Helmet>
 
-      <section className="mx-10">
+      <section className="mx-10 pt-10">
         <TitleButton title="나의 제주" link="#" />
         <hr />
         <ul className="flex grow gap-5 my-10 w-11/12 mx-auto">
-          <MainContent
-            src="/jejuImage1.jpg"
-            alt="메밀꽃밭"
-            title="제주 메밀꽃"
-          />
-          <MainContent src="/jejuImage4.jpg" alt="돌담" title="제주 돌담" />
-          <MainContent
-            src="/jejuImage3.jpg"
-            alt="한라봉 과수원"
-            title="제주는 한라봉이면 충분하다고 할 수 있지 않을까?"
-          />
+          {/* <MainContent /> */}
         </ul>
       </section>
 
