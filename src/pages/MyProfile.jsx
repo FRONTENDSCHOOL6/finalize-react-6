c9c2b306271119a807cb422e09babdf97a342a6c
rustandbone <dlehdgh14@naver.com>
2023-09-17 02:27:28 +0900

    feat: 프로필 페이지 더보기 버튼 구현
    related to: #42

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,18 +1,53 @@
 import pb from '@/api/pocketbase';
 import PageHead from '@/components/PageHead';
 import ProfileComment from '@/components/ProfileComment';
+import ScrollToTop from '@/components/ScrollToTop';
+import Spinner from '@/components/Spinner';
 import TitleButton from '@/components/TitleButton';
 import ContentItem from '@/components/content/ContentItem';
 import { useAuthStore } from '@/store/useAuthStore';
 import { getPbImageURL } from '@/utils';
+import { useQuery } from '@tanstack/react-query';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+async function fetchContents(options) {
+  let queryParams = '';
+  queryParams = `?sort=${options.sort}&filter=${options.filter}&page=${options.page}&perPage=${options.perPage}`;
+  const response = await fetch(
+    `${import.meta.env.VITE_PB_API}/collections/${
+      options.collection
+    }/records${queryParams}`
+  );
+  return await response.json();
+}
 
 export default function MyProfile() {
   const { user } = useAuthStore();
   const [nickname, setNickname] = useState('');
   const [post, setPost] = useState([]);
   const [comment, setComment] = useState([]);
+  const [showAllContent, setShowAllContent] = useState(false);
+  const [showAllComment, setShowAllComment] = useState(false);
+  const [collection, setCollection] = useState('content');
+
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(9);
+
+  const [sort, setSort] = useState('-created');
+
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['contents', sort, user, page, perPage, collection],
+    queryFn: () =>
+      fetchContents({
+        sort,
+        filter: `userId='${user.id}'`,
+        page,
+        perPage,
+        collection,
+      }),
+    keepPreviousData: true,
+  });
 
   useEffect(() => {
     async function getContent() {
@@ -42,8 +77,18 @@ export default function MyProfile() {
     getContent();
   }, [user.id]);
 
+  const handleShowAll = (category) => () => {
+    if (category === 'content') setShowAllContent(!showAllContent);
+    if (category === 'comment') setShowAllComment(!showAllComment);
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
+      <ScrollToTop />
       <PageHead title="Jeju All in One - 내 프로필" />
 
       <section className="pt-10 font-bold text-lg text-center">
@@ -51,21 +96,41 @@ export default function MyProfile() {
       </section>
 
       <section className="mx-10 pt-10">
-        <TitleButton title="나의 제주" link="#" count={comment.length} />
+        <TitleButton
+          title="나의 제주"
+          link="#"
+          count={post.length}
+          section="게시글"
+          onClick={handleShowAll('content')}
+          showMore={showAllContent}
+        />
         <hr />
         <ul className="my-10 w-11/12 mx-auto contentContainer">
-          {post?.length === 0 && <ContentItem />}
-          {post?.slice(0, 3).map((item) => {
-            return (
-              <ContentItem
-                key={item.id}
-                content={item.id}
-                title={item.title}
-                count={item.commentId.length}
-                src={getPbImageURL(item, 'photo')}
-              />
-            );
-          })}
+          {data?.items?.length === 0 && <ContentItem />}
+          {!showAllContent &&
+            data?.items?.slice(0, 3).map((item) => {
+              return (
+                <ContentItem
+                  key={item.id}
+                  content={item.id}
+                  title={item.title}
+                  count={item.commentId.length}
+                  src={getPbImageURL(item, 'photo')}
+                />
+              );
+            })}
+          {showAllContent &&
+            data?.items?.map((item) => {
+              return (
+                <ContentItem
+                  key={item.id}
+                  content={item.id}
+                  title={item.title}
+                  count={item.commentId.length}
+                  src={getPbImageURL(item, 'photo')}
+                />
+              );
+            })}
         </ul>
       </section>
 
@@ -75,6 +140,8 @@ export default function MyProfile() {
           link="#"
           count={comment.length}
           section="댓글"
+          onClick={handleShowAll('comment')}
+          showMore={showAllComment}
         />
         <hr />
         <ul className="w-11/12 mx-auto my-10">
@@ -90,18 +157,32 @@ export default function MyProfile() {
               </Link>
             </>
           )}
-          {comment?.map((item) => {
-            return (
-              <Link to={`/content/${item.contentId}`} key={item.id}>
-                <ProfileComment
-                  src={getPbImageURL(item.expand.contentId, 'photo')}
-                  alt={item.title}
-                  date={item.updated.split(' ')[0]}
-                  comment={item.comment}
-                />
-              </Link>
-            );
-          })}
+          {!showAllComment &&
+            comment?.slice(0, 5).map((item) => {
+              return (
+                <Link to={`/content/${item.contentId}`} key={item.id}>
+                  <ProfileComment
+                    src={getPbImageURL(item.expand.contentId, 'photo')}
+                    alt={item.title}
+                    date={item.updated.split(' ')[0]}
+                    comment={item.comment}
+                  />
+                </Link>
+              );
+            })}
+          {showAllComment &&
+            comment?.map((item) => {
+              return (
+                <Link to={`/content/${item.contentId}`} key={item.id}>
+                  <ProfileComment
+                    src={getPbImageURL(item.expand.contentId, 'photo')}
+                    alt={item.title}
+                    date={item.updated.split(' ')[0]}
+                    comment={item.comment}
+                  />
+                </Link>
+              );
+            })}
         </ul>
       </section>
     </>
