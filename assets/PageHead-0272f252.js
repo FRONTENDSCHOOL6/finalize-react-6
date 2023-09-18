78d427042a3eb3b6055ab51b4c066c2b665afc3f
rustandbone <dlehdgh14@naver.com>
2023-09-18 10:09:45 +0900

    Updates

--- assets/PageHead-0272f252.js
+++
@@ -1,1 +0,0 @@
-import{j as e,W as s,a as t}from"./vendor-544b9a11.js";function i({title:r}){return e.jsx(s,{children:e.jsx("title",{children:r})})}i.propTypes={title:t.string.isRequired};export{i as P};
