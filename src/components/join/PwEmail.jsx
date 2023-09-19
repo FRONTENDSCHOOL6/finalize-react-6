f0f8258c7ee18832f9eb476bf18a2183774f9eeb
miinje <njjj00083@gmail.com>
2023-09-19 11:08:04 +0900

    markup: PwEmail 마크업 수정

--- src/components/join/PwEmail.jsx
+++ src/components/join/PwEmail.jsx
@@ -7,10 +7,10 @@ export default function PwEmail() {
       <SubLogo />
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-xl text-darkblue font-bold mb-2">
-          메일을 확인해 주세요!
+          이메일을 확인해 주세요!
         </h2>
         <p className="text-lg mb-9">
-          적어 주신 이메일로 비밀번호 재설정 메일을 전송했습니다.
+          적어 주신 메일 주소로 비밀번호 재설정 이메일을 전송했습니다.
         </p>
         <p className="text-sa">
           <LinkItem link="/login" className="font-extrabold text-blue">
