abebdaaf4b38f95678720f32b7c1fbb0e6a05f90
rustandbone <dlehdgh14@naver.com>
2023-09-21 17:35:05 +0900

    style: s 미디어쿼리 조정

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ export default {
         KakaoYellow: '#FEE500',
       },
       screens: {
-        s: { min: '320px', max: '767px' },
+        s: { min: '320px', max: '1024px' },
         mobile: { min: '62.5em' },
       },
       boxShadow: {
