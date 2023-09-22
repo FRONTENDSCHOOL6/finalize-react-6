85a955b4b7a495cb65f3e87ac63b84b7a03f3985
haha41 <thgml500@gmail.com>
2023-09-22 11:22:35 +0900

    feat: 메인 페이지 텐스텍쿼리 적용
    related to: #40

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,39 +4,38 @@ import MainSlide from '@/components/MainSlide';
 import MainTag from '@/components/MainTag';
 import Spinner from '@/components/Spinner';
 import TitleButton from '@/components/TitleButton';
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
+async function getContentList() {
+  try {
+    const contentList = await pb.collection('content').getFullList({
+      expand: 'comment',
+    });
+    return contentList; // 쿼리 함수에서 데이터를 반환
+  } catch (error) {
+    throw error;
+  }
+}
+
 export default function Home() {
   const [page, setPage] = useState(1);
-  const [data, setData] = useState(null);
-  const [status, setStatus] = useState('pending');
   const [selectedTag, setSelectedTag] = useState(null);
 
-  useEffect(() => {
-    async function getContentList() {
-      try {
-        setStatus('loading');
-        const contentList = await pb.collection('content').getFullList({
-          expand: 'comment',
-        });
-        setData(contentList);
-        setStatus('success');
-      } catch (error) {
-        setStatus('error');
-      }
-    }
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['mainContents'],
+    queryFn: getContentList,
+    staleTime: 60 * 60 * 1000,
+  });
 
-    getContentList();
-  }, [page]);
+  if (isLoading) {
+    return <Spinner className="mx-auto" />;
+  }
 
-  if (!data) {
-    return (
-      <div className="grid place-content-center h-[100vh]">
-        <Spinner size={160} />
-      </div>
-    );
+  if (isError) {
+    return <div role="alert">{error.toString()}</div>;
   }
 
   const filteredData = selectedTag
