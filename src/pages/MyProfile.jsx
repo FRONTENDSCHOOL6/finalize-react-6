4220979ee769b9a2afee586422347a4e9f43dc94
rustandbone <dlehdgh14@naver.com>
2023-09-28 17:21:39 +0900

    feat: 프로필 페이지 닉네임 수정 기능
    resolves: #217

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -3,11 +3,14 @@ import PageHead from '@/components/PageHead';
 import ProfileCommentSection from '@/components/profile/ProfileCommentSection';
 import ProfileContentSection from '@/components/profile/ProfileContentSection';
 import { useAuthStore } from '@/store/useAuthStore';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import toast from 'react-hot-toast';
 
 export default function MyProfile() {
   const { user } = useAuthStore();
+  const nicknameRef = useRef(null);
   const [nickname, setNickname] = useState('');
+  const [nicknameEdit, setNicknameEdit] = useState(false);
   const [showAllContent, setShowAllContent] = useState(false);
   const [showAllComment, setShowAllComment] = useState(false);
 
@@ -21,16 +24,67 @@ export default function MyProfile() {
         console.error(error);
       }
     }
-
     getContent();
   }, [user.id]);
+
+  const handleEditmode = () => {
+    setNicknameEdit(true);
+  };
+
+  const handleNickname = () => {
+    try {
+      if (nickname === nicknameRef?.current?.value) {
+        toast.error('닉네임을 수정해주세요.');
+        return;
+      } else {
+        pb.collection('user').update(user.id, {
+          nickname: nicknameRef.current.value,
+        });
+        setNickname(nicknameRef.current.value);
+        toast.success('닉네임이 수정되었습니다.');
+        setNicknameEdit(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <PageHead title="Jeju All in One - 내 프로필" />
 
-      <section className="pt-10 font-bold text-lg text-center">
-        {nickname} 님 환영합니다.
+      <section className="flex justify-center gap-4 pt-10 font-bold text-lg text-center">
+        <article>
+          {!nicknameEdit && (
+            <p>
+              {nickname}
+              <button onClick={handleEditmode}>
+                <img
+                  src="/pen_edit.svg"
+                  // src={`${import.meta.env.VITE_DOMAIN}/pen_edit.svg`}
+                  alt="닉네임 수정"
+                  className="w-6"
+                />
+              </button>
+              님 환영합니다.
+            </p>
+          )}
+          {nicknameEdit && (
+            <div>
+              <input
+                type="text"
+                ref={nicknameRef}
+                defaultValue={nickname}
+                maxLength={10}
+                className="w-44 border-2 border-slate-500 rounded pl-2 mr-2"
+              />
+              <button onClick={handleNickname} className="mr-2">
+                수정
+              </button>
+              <button onClick={() => setNicknameEdit(false)}>취소</button>
+            </div>
+          )}
+        </article>
       </section>
 
       <ProfileContentSection
