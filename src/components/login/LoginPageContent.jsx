377fa417ed48cde7c19ee9c7ca43687308e7572b
rustandbone <dlehdgh14@naver.com>
2023-10-03 00:09:13 +0900

    design: height 최솟값 적용

--- src/components/login/LoginPageContent.jsx
+++ src/components/login/LoginPageContent.jsx
@@ -1,6 +1,6 @@
 import { node } from 'prop-types';
 export default function LoginPageContent({ children }) {
-  return <div className="flex flex-col">{children}</div>;
+  return <div className="flex flex-col min-h-[60vh]">{children}</div>;
 }
 
 LoginPageContent.propTypes = {
